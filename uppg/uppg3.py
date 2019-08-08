5639313c6bac600013659c138b6f42ea7da5c567
unknown <jonssonanton@hotmail.com>
2019-08-08 13:56:01 +0200

    Uppg3

--- uppg/uppg3.py
+++ uppg/uppg3.py
@@ -4,5 +4,6 @@ for varv in range(1,4):
 	print("Skriv en frukt")
 	frukter.append(input().title())
 
-
+for index, frukt in enumerate(frukter):
+	print("På plats " + str(index) +  " finns " + frukt + ".")
 
