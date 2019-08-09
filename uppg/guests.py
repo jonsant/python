abaa7246416e6b2491cd1f89e6d006ae47a378ee
unknown <jonssonanton@hotmail.com>
2019-08-09 15:09:25 +0200

    guests.py update

--- uppg/guests.py
+++ uppg/guests.py
@@ -4,8 +4,8 @@ run = True
 
 while run:
 	choice = input("1: Add guest, 2: Arrived, 3: quit\n")
-	print(choice)
 	
+	#Add
 	if choice == "1":
 		name = input("Name? ")
 		food = input("Food? ")
@@ -17,17 +17,23 @@ while run:
 			"drink": drink, "other": other, "arrived": False})
 		print(name + " added!\n")
 	
+	#Arrived
 	elif choice == "2":
-		name = input("Name? ")
-		print("\n")
-		for guest in guests:
-			if guest["name"] == name:
-				guest["arrived"] = True
-				for key, info in guest.items():
-					if key == "arrived":
-						continue
-					print(key + " : " + info)
-				print("welcome!\n")
-	
+		if guests:
+			name = input("Name? ")
+			print("\n")
+			for guest in guests:
+				if guest["name"] == name:
+					guest["arrived"] = True
+					for key, info in guest.items():
+						if key == "arrived":
+							continue
+						print(key + " : " + info)
+					print("welcome!\n")
+				
+		else:
+			print("Empty!\n")
+	#Quit
 	elif choice == "3":
+		print("Bye!")
 		run = False
