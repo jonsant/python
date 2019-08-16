6a986303cd3937f1f1d7b587c945931a1f2e90bb
Anton Thunman Jonsson <jonssonanton@hotmail.com>
2019-08-16 12:40:40 +0200

    invasion, movement, ship range

--- invasion/invasion.py
+++ invasion/invasion.py
@@ -1,22 +1,26 @@
-import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
-	# Initialize game and create screen object
+	# Initialize game, settings and screen object.
 	pygame.init()
-	screen = pygame.display.set_mode((1200,800))
+	
+	my_settings = Settings()
+	
+	screen = pygame.display.set_mode((my_settings.screen_width, my_settings.screen_height))
 	pygame.display.set_caption("Invasion")
+	
+	# Make a ship
+	ship = Ship(my_settings, screen)
 	
 	# Start the main loop for the game
 	while True:
 		
-		# Watch for keyboard and mouse events
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-				
-		# Make the ost recently drawn screen visible
-		pygame.display.flip()
+		gf.check_events(ship)
+		ship.update()
+		gf.update_screen(my_settings, screen, ship)
 		
 		
 run_game()
