960cf2a0ffa4b005c805b5380a91fe4664a3d797
Anton Thunman Jonsson <jonssonanton@hotmail.com>
2019-08-27 18:31:10 +0200

    Removed learning_log

--- learning_log/ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\anton\documents\kod\python\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
